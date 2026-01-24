2bd59125678a4d3ce297990842f02ac5565a3f98
yassinechouket <medyassinechouket.2@gmail.com>
2026-01-25 00:25:13 +0100

    Neon auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { StackProvider, StackTheme } from "@stackframe/stack";
+import { stackClientApp } from "../stack/client";
 import { Geist, Geist_Mono } from "next/font/google";
 import { NavBar } from "@/components/nav/nav-bar";
 import "./globals.css";
@@ -29,10 +31,10 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      ><StackProvider app={stackClientApp}><StackTheme>
         <NavBar />
         {children}
-      </body>
+      </StackTheme></StackProvider></body>
     </html>
   );
 }
